57f2ad6ee27b2d6f0fc5b17f38bd45a7eb5b48b4
Yessenia Martinez <l21211990@tectijuana.edu.mx>
2022-06-01 11:18:12 -0700

    Version de clase

---
+++ EscrituraArchivos-MtzBlanca/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EscrituraArchivos_MtzBlanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("Ejemplo.txt", true);
+
+            string[] lines = {
+                "\nesto es una nueva linea" +
+                "\nsgunda linea" +
+                "\ntecera linea"
+            };
+
+            foreach(string line in lines) {
+                sw.WriteLine(line);
+                    }
+
+            sw.Close();
+            Console.WriteLine("escribiendo");
+            Console.ReadLine();
+
+        }
+    }
+}
